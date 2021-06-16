45b7016e9d30f9b8f205db750c9471f87226f6d0
Rayne-Shadow-Legends <thegoldenbella22@gmail.com>
2021-06-15 20:28:54 -0700

    added a check to see if players input is a letter and warnings if it isnt or is longer than one character

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let guessedLetters = document.querySelector(".guessed-letters")
+let guessedLettersElement = document.querySelector(".guessed-letters")
 let guessButton = document.querySelector(".guess");
 let input = document.querySelector(".letter")
 let wordInProgress = document.querySelector(".word-in-progress");
@@ -6,7 +6,9 @@ let guessesParagraph = document.querySelector(".remaining");
 let remainignGuesses = document.querySelector(".remaining span")
 let message = document.querySelector(".message");
 let playAgain = document.querySelector(".play-again");
+
 let word = "mongolia";
+let guessedLetters = [];
 
 function hideWord() {
     for(hiddenWord = ""; hiddenWord.length < word.length; wordInProgress.innerText = hiddenWord) {
@@ -19,6 +21,33 @@ hideWord();
 guessButton.addEventListener("click", function(e){
     e.preventDefault();
     let playerGuess = input.value;
-    input.value = "";
-    console.log(playerGuess);
-})
+    message.innerText = "";
+    let validatedGuess = validatePlayerInput(playerGuess);
+
+    if(validatedGuess) {
+        makeGuess(validatedGuess)
+    }
+});
+
+let validatePlayerInput = function(input) {
+    let acceptedLetters = /[a-zA-Z]/
+    if(input.length === 0) {
+       message.innerText = "Please input a letter"
+    } else if(input.length > 1) {
+       message.innerText = "Only one letter is allowed"
+    } else if(!input.match(acceptedLetters)) {
+        message.innerText = "Only letters are allowed"
+    } else {
+        return input;
+    }
+};
+
+let makeGuess = function(letter) {
+    letter.toUpperCase();
+    if(guessedLetters.includes(letter)){
+        message.innerText = "You already guessed that letter";
+    } else {
+        guessedLetters.push(letter);
+        console.log(guessedLetters);
+    };
+};
